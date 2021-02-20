5e5714291c323b07fdb289e551deca60a04d1cb3
Xeltica <xeltica@gmail.com>
2021-02-20 22:24:34 +0900

    旅行

--- src/components/MinecraftStats.vue
+++ src/components/MinecraftStats.vue
@@ -3,12 +3,15 @@
 		<dt>ユーザー数</dt>
 		<dd v-text="players.length" />
 	</dl>
-	<ul class="users" v-for="item in players" :key="item">
+	<ul class="users" v-for="item in featuredPlayers" :key="item.uuid">
 		<li class="user">
-			<img class="avatar" :src="`https://crafatar.com/avatars/${item.uuid}?overlay`"/>
+			<img class="avatar" :src="`https://crafatar.com/avatars/${item.uuid}?overlay`" :alt="item.displayName" :title="item.displayName"/>
 			<span class="name" v-text="item.displayName"/>
 		</li>
 	</ul>
+	<div class="faces">
+		<img v-for="item in otherPlayers" :key="item.uuid" class="avatar" :alt="item.displayName" :title="item.displayName" :src="`https://crafatar.com/avatars/${item.uuid}?overlay`"/>
+	</div>
 	<div v-if="hasError" class="error">
 		取得に失敗しました。
 		<button @click="fetchPlayers">再試行</button>
@@ -16,7 +19,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, onUnmounted, reactive, ref } from 'vue';
+import { computed, defineComponent, onMounted, onUnmounted, reactive, ref } from 'vue';
 
 import { players, hasError, isFetching, fetchPlayers } from '../player';
 
@@ -28,6 +31,8 @@ export default defineComponent({
 	setup() {
 		return {
 			players,
+			featuredPlayers: computed(() => players.value.slice(0, 5)),
+			otherPlayers: computed(() => players.value.slice(5)),
 			hasError,
 			isFetching,
 			fetchPlayers,
@@ -59,14 +64,22 @@ h2 {
 		padding: 0 8px;
 		display: flex;
 		align-items: center;
-		> .avatar {
-			width: 1.4rem;
-			height: 1.4rem;
-			border-radius: 100%;
-		}
 		> .name {
 			margin-left: 16px;
 		}
 	}
+}
+
+.faces {
+	padding: 4px;
+	> .avatar {
+		margin: 4px;
+	}
+}
+
+.avatar {
+	width: 1.4rem;
+	height: 1.4rem;
+	border-radius: 100%;
 }
 </style>
