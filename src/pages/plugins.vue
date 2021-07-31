72325f01173df0a145e73dabcac2d7b23c5637a1
Xeltica <xeltica@gmail.com>
2021-07-31 11:47:50 +0900

    導入プラグイン一覧のデザイン修正

--- src/pages/plugins.vue
+++ src/pages/plugins.vue
@@ -1,27 +1,22 @@
 <template>
 	<section>
 		<h2>導入プラグイン</h2>
-		<table v-if="plugins" class="table">
-		<thead>
-			<tr>
-				<th />
-				<th>名前</th>
-				<th>バージョン</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr v-for="plugin in plugins" :key="plugin.name">
-				<td class="indicator" :class="plugin.enabled ? 'enabled' : 'disabled'">●</td>
-				<td v-text="plugin.name" />
-				<td v-text="plugin.version" />
-			</tr>
-		</tbody>
-		</table>
-		<div v-else-if="hasError" class="error">
-			取得に失敗しました。
-			<button @click="fetchPlayers">再試行</button>
+		<div v-if="plugins && plugins.length > 0" class="vgroup">
+			<div v-for="plugin in plugins" :key="plugin.name" class="card item">
+				<div class="body">
+					<h1 style="font-size: 1rem">
+						{{plugin.name}}
+						<span :class="plugin.enabled ? 'text-green' : 'text-red'" v-text="plugin.enabled ? '有効' : '無効'"/>
+					</h1>
+					<p v-text="plugin.version" class="text-dimmed" />
+				</div>
+			</div>
 		</div>
-		<div v-else class="loading">
+		<div v-else-if="hasError" class="text-red">
+			<p>取得に失敗しました。</p>
+			<button class="btn" @click="fetchPlugins">再試行</button>
+		</div>
+		<div v-else class="loading text-dimmed">
 			取得中…
 		</div>
 	</section>
@@ -31,7 +26,7 @@
 import { useHead } from '@vueuse/head';
 import { defineComponent } from 'vue';
 
-import { plugins, isFetching, hasError } from '../plugins';
+import { plugins, isFetching, fetchPlugins, hasError } from '../plugins';
 
 export default defineComponent({
 	name: 'Plugins',
@@ -43,6 +38,7 @@ export default defineComponent({
 		return {
 			plugins,
 			isFetching,
+			fetchPlugins,
 			hasError,
 		};
 	}
@@ -50,22 +46,17 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.error {
-	color: red;
-	font-weight: bold;
-}
 
 table {
 	width: 100%;
 }
 
-.indicator {
-	&.disabled { color: #f44336; }
-	&.enabled { color: #4CAF50; }
-}
-
 .loading {
 	font-style: oblique;
 	opacity: 0.7;
+}
+
+.card.item:not(:last-child) > .body {
+	padding-bottom: 0;
 }
 </style>
